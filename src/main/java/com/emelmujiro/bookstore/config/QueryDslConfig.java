55ab077dbe6048fbe893312357f7327b717becef
YOHAN <jkim6115@gmail.com>
2023-05-01 20:45:03 +0900

    2023-05-01 | jpa format_sql 설정 추가, querydsl config 추가

---
+++ src/main/java/com/emelmujiro/bookstore/config/QueryDslConfig.java
@@ -0,0 +1,24 @@
+package com.emelmujiro.bookstore.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+@NoArgsConstructor
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(this.entityManager);
+    }
+
+}
